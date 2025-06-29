8b92af9dc7c12ac117da8c2704f6831ffbd6bbdf
Angga7Togk <kiplihode123321@gmail.com>
2025-06-29 13:57:17 +0700

    fix middleware

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -2,6 +2,6 @@ import type { AppConfig, Handler, NextResponse } from "../types";
 
 export function defineMiddleware<A extends AppConfig = AppConfig>(
   handler: Handler<A>
-): NextResponse<A> {
+): Handler<A> {
   return handler;
 }

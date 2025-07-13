351745aa6e06880949755fae1ec939b43a9fa5a0
Angga7Togk <kiplihode123321@gmail.com>
2025-07-13 12:21:52 +0700

    feat: added dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export * from "./types";
 export * from "./gaman-base";
 export * from "./context";
+export * from "./cookies";
 export * from "./next";
 export * from "./response";
 export * from "./web-socket";
@@ -9,13 +10,16 @@ export * from "./middleware";
 export * from "./error/HttpError";
 export * from "./block";
 export * from "./utils/logger";
-export * from "./utils/cookie";
 export * from "./utils/color";
 export * from "./utils/form-data";
-export * from "./utils/headers";
+export * from "./headers";
 export * from "./integration";
 
+
 import { serv } from "./gaman";
+import dotenv from "dotenv";
+dotenv.config();
+
 
 export default {
   serv,

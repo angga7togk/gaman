970a236c30ad33ee76e3e5312eb410dda3576788
Angga7Togk <kiplihode123321@gmail.com>
2025-07-13 12:27:21 +0700

    feat: added symbol

---
+++ src/symbol.ts
@@ -0,0 +1,3 @@
+export const RESPONSE_RENDER_SYMBOL = Symbol.for("gaman.responseRender"),
+  HTTP_REQUEST_SYMBOL = Symbol.for("gaman.httpRequest"),
+  HTTP_RESPONSE_SYMBOL = Symbol.for("gaman.httpResponse");
